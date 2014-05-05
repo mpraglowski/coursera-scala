13f99e99fe58ca38ad3933f297ed1cb680367c30
Mirosław Pragłowski <m@praglowski.com>
2014-05-06 00:28:20 +0200

    added exc. 1 & 2 from recfun (done) and exc. 3 but not compiling yet

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,35 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = 
+    if (c == 0 || c == r ) 1
+    else pascal(c-1,r-1)+pascal(c,r-1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balanceIter(opened: Int, chars: List[Char]): Boolean =
+      if (opened < 0) false
+      else
+        if (chars.isEmpty) opened == 0
+        else chars.head match {
+          case '(' => balanceIter(opened+1, chars.tail)
+          case ')' => balanceIter(opened-1, chars.tail)
+          case _ => balanceIter(opened, chars.tail)
+        }
+    balanceIter(0,chars)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def countChangeIter(money: Int, coins: List[Int]) : Int
+      if (money == 0 ) 1
+      else if (money < 0) 0
+        else if (coins.isEmpty && money >= 1) 0
+          else countChangeIter(money, coins.tail) + countChangeIter(money-coins.head, coins)
+    countChangeIter(money,coins.sortWith(_ < _))
+  }
 }
